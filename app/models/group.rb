0efaabead516cec3125af93c5c9dbc16bfc53ce4
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-05-20 16:41:44 -0500

    test cases for group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,10 @@ class Group < ApplicationRecord
   has_many :wishlists_groups
   has_many :wishlists, -> { distinct }, through: :wishlists_groups, dependent: :destroy
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+
+  validates :name, presence: { message: 'Group name must be given' }
+  validates :icon, presence: { message: 'Icon must be given' }
+  validates :name, uniqueness: { message: 'Group name already exists' }
+  validates :name, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Group name must be alphanumeric' }
+  validates :name, length: { in: 1..50, message: 'Group name length must be between 1 to 50 characters' }
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ' model test cases ' do
+    fixtures :users
+    fixtures :wishlists
+    fixtures :groups
+
+    context 'validates field' do
+      it 'name must be valid' do
+        expect(groups(:one)).to be_valid
+      end
+      it 'name must not be valid' do
+        groups(:one).name = nil
+        expect(groups(:one)).to_not be_valid
+      end
+
+      it 'icon must not be valid' do
+        groups(:one).icon = nil
+        expect(groups(:one)).to_not be_valid
+      end
+
+      it 'validates user already exists' do
+        group_new = Group.new(name: 'Magpies', icon: "fab fa-500px", user_id: 1)
+        group_new.valid?
+        expect(group_new.errors[:name]).to include('Group name already exists')
+      end
+    end
+
+    context 'must include the message' do
+      it 'length between 1 to 50 characters' do
+        groups(:one).name = ''
+        groups(:one).valid?
+        expect(groups(:one).errors[:name]).to include('Group name length must be between 1 to 50 characters')
+      end
+      it 'must be alphanumeric' do
+        groups(:one).name = ' ruben22123123'
+        groups(:one).valid?
+        expect(groups(:one).errors[:name]).to include('Group name must be alphanumeric')
+      end
+    end   
+  end
 end
