2c1a5b3d599e27f729030566a9c00003c52527c3
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-05-23 11:43:57 -0500

    aff height size for navigation abr

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'application#home'
   
   resources :users
+  get     '/external_wishlists',  to: 'wishlists#list_external'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # wishlist
