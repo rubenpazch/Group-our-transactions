1c4d8879b1f081bd2afcde2e38bb0da7af2133e5
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-05-24 16:22:08 -0500

    changes routes file for groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
-  resources :groups  
+  
   root 'application#home'
+  # group
+  resources :groups, only: [:index, :show, :new, :edit, :create, :update]  
+  
   
   resources :users
   get     '/external_wishlists',  to: 'wishlists#list_external'
